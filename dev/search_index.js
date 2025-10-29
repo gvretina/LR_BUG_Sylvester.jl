7fc73f5da2855723bdac9c6a76608831651bba94
Documenter.jl <documenter@juliadocs.github.io>
2025-10-29 17:32:44 +0000

    build based on d9710e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LR*BUG*Sylvester","page":"Home","title":"LRBUGSylvester","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LRBUGSylvester.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
